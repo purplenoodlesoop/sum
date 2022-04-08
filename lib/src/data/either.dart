f5f2c0970cb1570abcb56ff5ccef74abb7456da3
Yakov Karpov <yakovkarpoff@gmail.com>
2022-04-09 02:12:28 +0300

    Add Either

---
+++ lib/src/data/either.dart
@@ -0,0 +1,212 @@
+abstract class Either<L, R> {
+  const Either._();
+
+  const factory Either.left(L left) = Left;
+
+  const factory Either.right(R right) = Right;
+
+  L? get left;
+
+  R? get right;
+
+  bool get isLeft;
+
+  bool get isRight;
+
+  Either<L, B> map<B>(B Function(R right) mapper);
+
+  Either<A, R> mapLeft<A>(A Function(L left) mapper);
+
+  Either<L, B> flatMap<B>(Either<L, B> Function(R right) mapper);
+
+  T match<T>({
+    required T Function(Left<L, R> either) left,
+    required T Function(Right<L, R> either) right,
+  });
+
+  T matchOr<T>({
+    T Function(Left<L, R> either)? left,
+    T Function(Right<L, R> either)? right,
+    required T Function() fallback,
+  });
+
+  T? matchOrNull<T>({
+    T Function(Left<L, R> either)? left,
+    T Function(Right<L, R> either)? right,
+  });
+
+  T when<T>({
+    required T Function(L left) left,
+    required T Function(R right) right,
+  });
+
+  T whenOr<T>({
+    T Function(L left)? left,
+    T Function(R right)? right,
+    required T Function() fallback,
+  });
+
+  T? whenOrNull<T>({
+    T Function(L left)? left,
+    T Function(R right)? right,
+  });
+}
+
+class Left<L, R> extends Either<L, R> {
+  @override
+  final L left;
+
+  const Left(this.left) : super._();
+
+  @override
+  R? get right => null;
+
+  @override
+  bool get isLeft => true;
+
+  @override
+  bool get isRight => false;
+
+  @override
+  Either<L, B> map<B>(B Function(R right) mapper) => Left(left);
+
+  @override
+  Either<A, R> mapLeft<A>(A Function(L left) mapper) => Left(mapper(left));
+
+  @override
+  Either<L, B> flatMap<B>(Either<L, B> Function(R right) mapper) => Left(left);
+
+  @override
+  T match<T>({
+    required T Function(Left<L, R> either) left,
+    required T Function(Right<L, R> either) right,
+  }) =>
+      left(this);
+
+  @override
+  T matchOr<T>({
+    T Function(Left<L, R> either)? left,
+    T Function(Right<L, R> either)? right,
+    required T Function() fallback,
+  }) =>
+      left?.call(this) ?? fallback();
+
+  @override
+  T? matchOrNull<T>({
+    T Function(Left<L, R> either)? left,
+    T Function(Right<L, R> either)? right,
+  }) =>
+      left?.call(this);
+
+  @override
+  T when<T>({
+    required T Function(L left) left,
+    required T Function(R right) right,
+  }) =>
+      left(this.left);
+
+  @override
+  T whenOr<T>({
+    T Function(L left)? left,
+    T Function(R right)? right,
+    required T Function() fallback,
+  }) =>
+      left?.call(this.left) ?? fallback();
+
+  @override
+  T? whenOrNull<T>({
+    T Function(L left)? left,
+    T Function(R right)? right,
+  }) =>
+      left?.call(this.left);
+
+  @override
+  String toString() => 'Left($left)';
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || (other is Left<L, R> && left == other.left);
+
+  @override
+  int get hashCode => left.hashCode;
+}
+
+class Right<L, R> extends Either<L, R> {
+  @override
+  final R right;
+
+  const Right(this.right) : super._();
+
+  @override
+  L? get left => null;
+
+  @override
+  bool get isLeft => false;
+
+  @override
+  bool get isRight => true;
+
+  @override
+  Either<L, B> map<B>(B Function(R right) mapper) => Right(mapper(right));
+
+  @override
+  Either<A, R> mapLeft<A>(A Function(L left) mapper) => Right(right);
+
+  @override
+  Either<L, B> flatMap<B>(Either<L, B> Function(R right) mapper) =>
+      mapper(right);
+
+  @override
+  T match<T>({
+    required T Function(Left<L, R> either) left,
+    required T Function(Right<L, R> either) right,
+  }) =>
+      right(this);
+
+  @override
+  T matchOr<T>({
+    T Function(Left<L, R> either)? left,
+    T Function(Right<L, R> either)? right,
+    required T Function() fallback,
+  }) =>
+      right?.call(this) ?? fallback();
+
+  @override
+  T? matchOrNull<T>({
+    T Function(Left<L, R> either)? left,
+    T Function(Right<L, R> either)? right,
+  }) =>
+      right?.call(this);
+
+  @override
+  T when<T>({
+    required T Function(L left) left,
+    required T Function(R right) right,
+  }) =>
+      right(this.right);
+
+  @override
+  T whenOr<T>({
+    T Function(L left)? left,
+    T Function(R right)? right,
+    required T Function() fallback,
+  }) =>
+      right?.call(this.right) ?? fallback();
+
+  @override
+  T? whenOrNull<T>({
+    T Function(L left)? left,
+    T Function(R right)? right,
+  }) =>
+      right?.call(this.right);
+
+  @override
+  String toString() => 'Right($right)';
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || (other is Right<L, R> && right == other.right);
+
+  @override
+  int get hashCode => right.hashCode;
+}
