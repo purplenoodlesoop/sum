24281feac6f5282de98fa5f34d535699127757df
Yakov Karpov <yakovkarpoff@gmail.com>
2022-04-09 19:51:03 +0300

    Prepare for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,19 @@
-## [0.1.0] - 09.04.2022
+## [0.2.0] - 09.04.2022
+
+### Added
+
+#### Async data
+
+- Expose `SessionAsyncData`
+- Expose `SessionAsyncDataMatcher`
+- Expose `OperationAsyncData`
+- Expose `PersistentAsyncData`
+
+#### Data
+
+- Expose `Either`
+- Expose `Maybe`
+
+## [0.1.0] - 08.04.2022
 
 - Initial version.
--- lib/sum.dart
+++ lib/sum.dart
@@ -1,1 +1,12 @@
 library sum;
+
+export 'src/async_data/operation_async_data.dart';
+
+export 'src/async_data/persistent_async_data.dart';
+
+export 'src/async_data/session_async_data/session_async_data.dart';
+export 'src/async_data/session_async_data/session_async_data_matcher.dart';
+
+export 'src/data/either.dart';
+
+export 'src/data/maybe.dart';
